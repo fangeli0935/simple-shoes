4e570b967e69510913d9c821ef72d4470a2c8efb
flaviano.angeli <flaviano.angeli@havan.com.br>
2024-04-25 09:24:44 -0300

    adicionados comandos DDL e DML

---
+++ Activities/DDL/Add_Cliente_Primary_Key.sql
@@ -0,0 +1,3 @@
+-- Adiciona PK na Cliente
+ALTER TABLE Cliente ADD CONSTRAINT [PK_Cliente_Codigo] PRIMARY KEY (Codigo);
+GO
---
+++ Activities/DML/Insert_Cliente_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO Cliente (Codigo,Nome,DataNascimento,Cpf,CidadeNascimento,EstadoNascimento,CidadeResidencia,EstadoResidencia,EndLogradouro,EndNumero,Cep)
+ VALUES (1, 'Genoveva Armindo da Silva'   , '1970-01-01', '109.135.618-90', 'Brusque','SC','Brusque','SC','Rua numero zero'  , 10, 10045678),
+        (2, 'João Cesar Augusto'          , '1990-04-07', '217.230.272-98', 'Brusque','SC','Brusque','SC','Rua numero um'    , 20, 13445679),
+		(3, 'Joana Almeida Santos'        , '1950-08-04', '325.339.374-70', 'Brusque','SC','Brusque','SC','Rua numero dois'  , 30, 12455973),
+		(4, 'Cleiton de Souza Prado'      , '2005-01-23', '434.438.473-96', 'Brusque','SC','Brusque','SC','Rua numero tres'  , 40, 12356648),
+		(5, 'Esmeraldina Silva e Lima'    , '1980-04-22', '543.537.528-50', 'Brusque','SC','Brusque','SC','Rua numero quatro', 50, 12746578),
+		(6, 'Augusto Pena'                , '1979-09-17', '652.636.674-94', 'Brusque','SC','Brusque','SC','Rua numero cinco' , 60, 12646788),
+		(7, 'Leopoldina de Bragança'      , '2006-05-11', '765.734.718-30', 'Brusque','SC','Brusque','SC','Rua numero seis'  , 70, 12375689),
+		(8, 'Cristóvan Buarque de Orleans', '1976-02-20', '871.835.874-92', 'Brusque','SC','Brusque','SC','Rua numero sete'  , 80, 12835679),
+		(9, 'Barbara Souto Coelho Campo'  , '2001-03-31', '985.425.978-10', 'Brusque','SC','Brusque','SC','Rua numero oito'  , 90, 19325678);
+GO
